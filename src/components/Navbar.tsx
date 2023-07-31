136899d3e4bdd9680d1c9247b79a19ef935be4c5
Amit Bhardwaj <mramaj647@gmail.com>
2023-07-31 22:49:12 +0530

    correct navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
             <nav className={styles.navbar }>
                 <ul className={styles.navbarul }>
                     <li className={styles.navbaritem }><a className={styles.navbarancher } href="/">Home</a></li>
-                    <li className={styles.navbaritem }><a className={styles.navbarancher}  href="/">About</a></li>
+                    {/* <li className={styles.navbaritem }><a className={styles.navbarancher}  href="/">About</a></li> */}
                 </ul>
                 <div className={styles.topright}> 
                     <a className={styles.toprighta} href="/profile/withluke">MyProfile</a>
